43b11a7952adcd7620f9e0a6df9ce7b23ebdc356
Williamliu <william_lwh@hotmail.com>
2015-02-07 14:36:41 -0800

    Init upload

--- WinApp3/Form1.cs
+++ WinApp3/Form1.cs
@@ -16,5 +16,10 @@ namespace WinApp3
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("hello world");
+        }
     }
 }
